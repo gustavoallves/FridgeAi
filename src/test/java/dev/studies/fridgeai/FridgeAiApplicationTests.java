b74cb0af1881c6704d3f2888b2da18712dfb5d0d
Gustavo <gustavoallvesdev@gmail.com>
2025-07-19 19:44:23 -0300

    :tada: Inicializa a estrutura do projeto com configuração do Maven e Spring Boot

---
+++ src/test/java/dev/studies/fridgeai/FridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.studies.fridgeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
